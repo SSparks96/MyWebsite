a85e67e673394ee39ec69efd22f3965ff1a48d23
stephen sparks <sdsparks@mnu.edu>
2021-07-28 21:34:32 -0500

    adding unstaged files

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Contact
+from .serializers import ContactSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ContactListCreate(generics.ListCreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
